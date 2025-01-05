0c9542b8dbeb8bbef0bbc923cbe243a13983d82b
Kyel Harwell <138349999+quantumGrail@users.noreply.github.com>
2025-01-05 05:12:10 +0000

    Create templates

--- application/app.py
+++ application/app.py
@@ -1,10 +1,15 @@
 # Imports
+from cs50 import SQL
 from flask import Flask, render_template, jsonify
 
 # Configuration
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates/static')
 
 # Index Page
 @app.route('/')
 def index():
-    return "Welcome!"
+    return render_template("index.html")
+
+# Run Mode
+if __name__ == '__main__':
+    app.run(debug=True)
